8b2149c4fac7240908adb3634640e1d4094d865d
stephenpjara <Jara>
2021-07-06 16:23:08 -0700

    login function
    login button & authorization function

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,6 +5,7 @@ Page({
   data: {
     avatarUrl: './user-unlogin.png',
     userInfo: {},
+    username: '用户未登录',
     hasUserInfo: false,
     logged: false,
     takeSession: false,
@@ -14,43 +15,47 @@ Page({
   },
 
   onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true,
-      })
-    }
+    let that = this
+    // if (!wx.cloud) {
+    //   wx.redirectTo({
+    //     url: '../chooseLib/chooseLib',
+    //   })
+    //   return
+    // }
   },
 
-  getUserProfile() {
+  authorization() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
+        console.log(res.userInfo.avatarUrl)
         this.setData({
           avatarUrl: res.userInfo.avatarUrl,
           userInfo: res.userInfo,
           hasUserInfo: true,
+          logged: true,
+          username: res.userInfo.nickName
         })
       }
     })
   },
 
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true,
-      })
-    }
-  },
+
+  // onGetUserInfo: function(e) {
+  //   let that = this
+  //   if (!that.data.logged && e.detail.userInfo) {
+  //     console.log("on get user info")
+  //     that.setData({
+  //       logged: true,
+  //       avatarUrl: e.detail.userInfo.avatarUrl,
+  //       userInfo: e.detail.userInfo,
+  //       hasUserInfo: true,
+  //     })
+  //   }
+  // },
+
+
 
   onGetOpenid: function() {
     // 调用云函数
