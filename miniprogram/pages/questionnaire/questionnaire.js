2451bf4d45c3dd70ee5a5406c07cf03b3b74fc8e
Chris <chrisms0921@gmail.com>
2021-08-25 10:20:10 -0700

    now we can retrieve users' tapped answer and save it to an array -chris

--- miniprogram/pages/questionnaire/questionnaire.js
+++ miniprogram/pages/questionnaire/questionnaire.js
@@ -2,21 +2,34 @@ const app = getApp();
 
 Page({
   data: {
-    questions: [{question: '没有兴趣或不乐意做事情'}, 
-                {question: '感到情绪低落，沮丧或绝望'}, 
-                {question: '入睡或保持睡眠有困难'}, 
-                {question: '感到疲劳或没有精神'}, 
-                {question: '胃口不好或饮食过度'}, 
-                {question: '对自己感到难过'}],
-    options: [{option: '很少或没有 不到1天', value: '很少或没有 不到1天', check: true},
-              {option: '一些或一点 1-2天', value: '一些或一点 1-2天', check: false},
-              {option: '偶尔或适量 3-4天', value: '偶尔或适量 3-4天', check: false},
-              {option: '大部分或所有 5-7天', value: '大部分或所有 5-7天', check: false}]
+    questions: [{option: 0, value: '没有兴趣或不乐意做事情'}, 
+                {option: 1, value: '感到情绪低落，沮丧或绝望'}, 
+                {option: 2, value: '入睡或保持睡眠有困难'}, 
+                {option: 3, value: '感到疲劳或没有精神'}, 
+                {option: 4, value: '胃口不好或饮食过度'}, 
+                {option: 5, value: '对自己感到难过'}],
+    options: [{option: 0, value: '很少或没有 不到1天'},
+              {option: 1, value: '一些或一点 1-2天'},
+              {option: 2, value: '偶尔或适量 3-4天'},
+              {option: 3, value: '大部分或所有 5-7天'}],
+    answers: ["", "", "", "", "", ""],
   },
 /////////////////////////// "Submit" - In progress
   radio_data: {
     highest: 18.0, // 3 * 6 = 18
     scores: [0, 0, 0, 0, 0, 0] // to store the score for each question
+  },
+
+  test(event) {
+    console.log(event.currentTarget.dataset.index);
+    console.log(event.mark.groupMark);
+    console.log("tapped");
+
+    let questionNum = event.mark.groupMark
+    let answerNum = event.currentTarget.dataset.index;
+
+    this.data.answers[questionNum] = this.data.options[answerNum].value;
+    
   },
 
   radio_change(e) {
