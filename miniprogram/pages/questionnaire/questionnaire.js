5fb9a8d0a66fb198b5889f825c65b4f7d249fa9d
stephenpjara <stephenpjara@gmail.com>
2021-09-01 10:22:03 -0700

    xx

--- miniprogram/pages/questionnaire/questionnaire.js
+++ miniprogram/pages/questionnaire/questionnaire.js
@@ -80,6 +80,7 @@ Page({
           }
           total += allScores[i]
         }
+
         var finalScore = Math.round(total)
         console.log(finalScore)
         console.log(this.data.answers)
