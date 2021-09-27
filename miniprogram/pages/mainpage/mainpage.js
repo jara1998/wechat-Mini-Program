9caf8eab3775235d3fd66252a7afc89020b12033
chutel <chutel@uw.edu>
2021-09-27 14:08:27 -0700

    heart button

--- miniprogram/pages/mainpage/mainpage.js
+++ miniprogram/pages/mainpage/mainpage.js
@@ -1,3 +1,66 @@
-const app = getApp()
+const app = getApp();
+// pages/mainpage/mainpage.js
+Page({
 
-Page({});
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+  
+  toInfoPage: function () {
+    wx.navigateTo({
+      url: '../infoPage/infoPage',
+    })
+  }
+})
