952ac237a7d16fba92203dac2018b798c06a4695
stephenpjara <stephenpjara@gmail.com>
2021-10-18 02:01:39 -0400

    Auto login Function
    Auto Login is completed the same time the App is launching instead of the specific mainpage is loaded. So, You will have the access to globalData even you don't compile the mainpage

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,7 +3,6 @@ App({
   globalData: {
     userData: {}
   },
-
   onLaunch: function () {
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
@@ -16,8 +15,38 @@ App({
         // env: 'my-env-id',
         traceUser: true,
       })
-    }
 
-    this.globalData = {}
-  }
+      var that = this
+      wx.showLoading({
+        title: '加载中',
+      })
+      wx.cloud.callFunction({
+        name: 'authorized_user',
+        data: {
+        },
+        success: out => {
+          console.log('callfunction sucess');
+          console.log(out);
+          if (out.result.errCode == 0) {
+            if (out.result.data.registered) {
+              that.globalData.userData = out.result.data.userData;
+              console.log(out.errMsg);
+            } else {
+              console.log(out.errMsg);
+            }
+          } else {
+            console.log(out.errMsg);
+          }
+        },
+        fail: out => {
+          console.log('call function failed')
+        },
+        complete: out => {
+          console.log('call function completed')
+          wx.hideLoading()
+          console.log(this.globalData.userData)
+        }
+      })
+      }
+    }
 })
